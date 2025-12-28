a85e7a7a83001ac4abab989098071b84e85526c5
Juanmanueloddone <juancho12oddone@gmail.com>
2025-12-27 21:07:47 -0300

    Update game.ts

--- game/core/game.ts
+++ game/core/game.ts
@@ -1,13 +1,19 @@
 import { initialGameState, GameState } from "../state/gameState";
 import { joinGame, rewardPlayer } from "../actions/playerActions";
 import { gameTick } from "../ticks/gameTick";
+import { loadGameState, saveGameState } from "../state/persist";
 
 let state: GameState = initialGameState;
 
-export function startGame(): GameState {
+export async function startGame(): Promise<GameState> {
+  const persisted = await loadGameState();
+  state = persisted ?? state;
+
   state = joinGame(state, "player-1");
   state = rewardPlayer(state, "player-1", 10);
   state = gameTick(state);
+
+  await saveGameState(state);
   return state;
 }
 
