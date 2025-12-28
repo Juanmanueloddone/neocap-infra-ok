4875d15179a6811ece10f5d4c412eec8c3b15550
Juanmanueloddone <juancho12oddone@gmail.com>
2025-12-27 21:17:24 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,30 @@
-import { startGame } from "../game/core/game";
+import { join, earn, spend } from "./actions/player";
+import { loadGameState } from "../game/state/persist";
 
 export default async function Home() {
-  const state = await startGame();
+  const state = await loadGameState();
 
   return (
     <main>
       <h1>NEOCAP</h1>
+
+      <form action={join}>
+        <input type="hidden" name="playerId" value="player-1" />
+        <button>Join</button>
+      </form>
+
+      <form action={earn}>
+        <input type="hidden" name="playerId" value="player-1" />
+        <input type="hidden" name="amount" value="5" />
+        <button>Earn 5 NEOC</button>
+      </form>
+
+      <form action={spend}>
+        <input type="hidden" name="playerId" value="player-1" />
+        <input type="hidden" name="amount" value="3" />
+        <button>Spend 3 NEOC</button>
+      </form>
+
       <pre>{JSON.stringify(state, null, 2)}</pre>
     </main>
   );
