08720588f8965d15449df66562d6b10792983ead
Juanmanueloddone <juancho12oddone@gmail.com>
2025-12-27 21:03:09 -0300

    Create gameState.ts

---
+++ game/state/gameState.ts
@@ -0,0 +1,16 @@
+import type { Wallet, PlayerId } from "../economy/neoc";
+
+export interface PlayerState {
+  id: PlayerId;
+  wallet: Wallet;
+}
+
+export interface GameState {
+  players: Record<PlayerId, PlayerState>;
+  tick: number;
+}
+
+export const initialGameState: GameState = {
+  players: {},
+  tick: 0,
+};
