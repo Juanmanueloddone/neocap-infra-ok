d1e5dd40420894c44b2a7d5824a8273e24d2d147
Juanmanueloddone <juancho12oddone@gmail.com>
2026-01-02 11:41:28 -0300

    Update route.ts

--- app/api/state/route.ts
+++ app/api/state/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { NextResponse } from "next/server";
 import { loadGameState } from "../../../game/state/persist";
 import { initialGameState } from "../../../game/state/gameState";
