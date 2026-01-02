accc2ee6e04c2e0abeeb7a652c097c5c336c0fdd
Juanmanueloddone <juancho12oddone@gmail.com>
2026-01-02 08:56:59 -0300

    Update route.ts

--- app/api/join/route.ts
+++ app/api/join/route.ts
@@ -1,8 +1,4 @@
-import { NextResponse } from "next/server";
-
-export async function POST() {
-  // más adelante: insertar en Supabase
-  console.log("Player joined");
-
-  return NextResponse.redirect(new URL("/", process.env.NEXT_PUBLIC_BASE_URL));
-}
+export const gameState = {
+  tick: 0,
+  players: [],
+};
